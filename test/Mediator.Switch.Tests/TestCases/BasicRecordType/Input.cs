6fb98c461c6296feac32002081ec350a24544767
Zach Saw <zach.saw@gmail.com>
2025-04-11 21:47:45 +1000

    Add unit test for record types

---
+++ test/Mediator.Switch.Tests/TestCases/BasicRecordType/Input.cs
@@ -0,0 +1,13 @@
+using Mediator.Switch;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests.BasicRecordType;
+
+[RequestHandler(typeof(PingHandler))]
+public record Ping : IRequest<string>;
+public record PingHandler : IRequestHandler<Ping, string>
+{
+    public Task<string> Handle(Ping request, CancellationToken cancellationToken = default) => Task.FromResult("Pong");
+}
