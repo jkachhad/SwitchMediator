dfc2a7eccdd420b48b3ec0a5546baab62ccff732
Zach Saw <zach.saw@gmail.com>
2025-04-14 23:37:43 +1000

    Lazy NotificationHandlers now registering correctly

--- src/Mediator.Switch.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Mediator.Switch.Extensions.Microsoft.DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,20 +1,19 @@
 using System.Linq.Expressions;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Mediator.Switch.Extensions.Microsoft.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddMediator<TSwitchMediator>(this IServiceCollection services, ServiceLifetime serviceLifetime, params Assembly[] assembliesToScan)
-        where TSwitchMediator : class, IMediator
-    {
-        return AddMediator<TSwitchMediator>(services, op =>
+        where TSwitchMediator : class, IMediator =>
+        AddMediator<TSwitchMediator>(services, op =>
         {
             op.TargetAssemblies = assembliesToScan;
             op.ServiceLifetime = serviceLifetime;
         });
-    }
 
     public static IServiceCollection AddMediator<TSwitchMediator>(this IServiceCollection services, Action<SwitchMediatorOptions>? configure)
         where TSwitchMediator : class, IMediator
@@ -27,95 +26,167 @@ public static class ServiceCollectionExtensions
         services.Add(new ServiceDescriptor(typeof(ISender), sp => sp.GetRequiredService<IMediator>(), options.ServiceLifetime));
         services.Add(new ServiceDescriptor(typeof(IPublisher), sp => sp.GetRequiredService<IMediator>(), options.ServiceLifetime));
 
-        // get all types from the target assemblies
-        var allTypes = options.TargetAssemblies.Where(assembly => assembly != null)
-            .SelectMany(assembly => assembly.GetTypes())
-            .ToArray();
-
-        // Register Handlers
-        var handlerTypes = allTypes
-            .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Any(i =>
-                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)))
+        var allTypes = options.TargetAssemblies
+            .Where(assembly => assembly != null)
+            .Distinct()
+            .SelectMany(assembly =>
+            {
+                try
+                {
+                    return assembly.GetTypes();
+                }
+                catch (ReflectionTypeLoadException)
+                {
+                    return [];
+                }
+            })
+            .Where(t => t is {IsClass: true, IsAbstract: false} &&
+                        t.GetInterfaces()
+                            .Any(i => i.IsGenericType &&
+                                      (i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>) ||
+                                       i.GetGenericTypeDefinition() == typeof(INotificationHandler<>) ||
+                                       i.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>))))
             .ToList();
 
-        foreach (var handlerType in handlerTypes)
-        {
-            services.Add(new ServiceDescriptor(handlerType, handlerType, options.ServiceLifetime));
-            services.Add(new ServiceDescriptor(typeof(Lazy<>).MakeGenericType(handlerType),
-                sp => CreateLazyService(sp, handlerType), options.ServiceLifetime));
-        }
-
-        // Register Notification Handlers (without explicit ordering initially)
-        var notificationHandlerTypes = allTypes
-            .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Any(i =>
-                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>)))
-            .ToList();
-
-        foreach (var handlerType in notificationHandlerTypes)
-        {
-            // Register the concrete type
-            services.Add(new ServiceDescriptor(handlerType, handlerType, options.ServiceLifetime));
-            services.Add(new ServiceDescriptor(typeof(Lazy<>).MakeGenericType(handlerType),
-                sp => CreateLazyService(sp, handlerType), options.ServiceLifetime));
-
-            // TODO
-            // Also register against notification handler interfaces
-            // foreach (var handlerInterface in handlerType.GetInterfaces()
-            //              .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>)))
-            // {
-            //     services.Add(new ServiceDescriptor(handlerInterface, sp => sp.GetRequiredService(handlerType), options.ServiceLifetime));
-            // }
-        }
-
-        // Register Pipeline Behaviors
-        var pipelineBehaviorTypes = allTypes
-            .Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Any(i =>
-                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>)))
-            .ToList();
-
-        foreach (var behaviorType in pipelineBehaviorTypes)
-        {
-            services.Add(new ServiceDescriptor(behaviorType, behaviorType, options.ServiceLifetime));
-        }
+        RegisterRequestHandlers(services, allTypes, options.ServiceLifetime);
+        RegisterNotificationHandlers(services, allTypes, options.ServiceLifetime);
+        RegisterPipelineBehaviors(services, allTypes, options.ServiceLifetime);
 
         return services;
     }
 
-    private static object CreateLazyService(IServiceProvider serviceProvider, Type type)
+    private static void RegisterRequestHandlers(IServiceCollection services, IEnumerable<Type> allTypes, ServiceLifetime lifetime)
     {
-        var lazyType = typeof(Lazy<>).MakeGenericType(type);
-        var funcType = typeof(Func<>).MakeGenericType(type);
-
-        // We need to create a Func<T> delegate dynamically that resolves the service.
-        // Expression trees are a good way to do this: () => serviceProvider.GetRequiredService<T>()
-
-        // 1. Get the MethodInfo for serviceProvider.GetRequiredService<T>()
-        // Note: GetRequiredService<T> is an extension method in ServiceProviderServiceExtensions
-        var getServiceMethod = typeof(ServiceProviderServiceExtensions)
-            .GetMethod(nameof(ServiceProviderServiceExtensions.GetRequiredService),
-                [typeof(IServiceProvider)])?
-            .MakeGenericMethod(type);
-
-        if (getServiceMethod == null) // Should not happen in standard scenarios
+        foreach (var handlerType in allTypes
+                     .Where(t => t.GetInterfaces().Any(i =>
+                         i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))))
         {
-            throw new InvalidOperationException(
-                $"Unable to find the GetRequiredService method for type {type.Name}.");
+            services.TryAdd(new ServiceDescriptor(handlerType, handlerType, lifetime));
+            services.TryAdd(new ServiceDescriptor(typeof(Lazy<>).MakeGenericType(handlerType),
+                BuildLazyFactoryDelegate(handlerType), lifetime));
+        }
+    }
+
+    private static void RegisterNotificationHandlers(
+        IServiceCollection services,
+        IEnumerable<Type> allTypes,
+        ServiceLifetime lifetime)
+    {
+        var notificationHandlerTypes = allTypes
+            .Where(t => t.GetInterfaces().Any(i =>
+                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>)))
+            .ToList();
+
+        // 1. Register individual concrete handlers
+        foreach (var handlerType in notificationHandlerTypes)
+        {
+            services.TryAdd(new ServiceDescriptor(handlerType, handlerType, lifetime));
         }
 
-        // 2. Build the expression tree
-        // Constant expression representing the serviceProvider instance passed to our factory
-        var providerConstant = Expression.Constant(serviceProvider);
-        // Call expression representing serviceProvider.GetRequiredService<handlerType>()
-        var callExpression =
-            Expression.Call(null, getServiceMethod, providerConstant); // It's a static extension method
-        // Lambda expression representing the Func<handlerType>
-        var lambdaExpression = Expression.Lambda(funcType, callExpression);
+        // 2. Group handlers by the specific notification type they implement
+        var handlersByNotificationType = notificationHandlerTypes
+            .SelectMany(handlerType => handlerType.GetInterfaces()
+                .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>))
+                .Select(i => (NotificationType: i.GetGenericArguments()[0], HandlerType: handlerType)))
+            .GroupBy(x => x.NotificationType)
+            .ToDictionary(g => g.Key, g => g.Select(x => x.HandlerType).Distinct().ToArray());
 
-        // 3. Compile the expression tree into an actual Func<T> delegate
-        var funcDelegate = lambdaExpression.Compile();
+        // 3. Get MethodInfo for the internal static OrderNotificationHandlers<TNotification> method
+        var orderMethodInfo = typeof(ServiceCollectionExtensions).GetMethod(nameof(OrderNotificationHandlers), BindingFlags.Static | BindingFlags.NonPublic);
+        if (orderMethodInfo == null)
+        {
+            throw new InvalidOperationException("Could not find the internal static 'OrderNotificationHandlers' method via reflection.");
+        }
 
-        // 4. Create the Lazy<handlerType> instance using its constructor that takes Func<T>
-        // Use Activator.CreateInstance as we don't know the type at compile time.
-        return Activator.CreateInstance(lazyType, funcDelegate)!;
+        // 4. Dynamically call the internal static method for each notification type
+        foreach (var kvp in handlersByNotificationType)
+        {
+            var notificationType = kvp.Key; // TNotification
+            var specificHandlerTypes = kvp.Value; // Type[] implementing INotificationHandler<TNotification>
+
+            try
+            {
+                // Create a closed generic method, e.g., OrderNotificationHandlers<MyNotification>
+                var concreteOrderMethod = orderMethodInfo.MakeGenericMethod(notificationType);
+
+                // Prepare arguments: IServiceCollection, Type[], ServiceLifetime
+                object[] methodArgs = [services, specificHandlerTypes, lifetime];
+
+                // Invoke the static method (instance is null)
+                concreteOrderMethod.Invoke(null, methodArgs);
+            }
+            catch (Exception ex) when (ex is TargetInvocationException or ArgumentException)
+            {
+                // Catch reflection or argument errors specifically
+                throw new InvalidOperationException($"Error registering ordered handlers for notification type '{notificationType.FullName}'. See inner exception.", ex);
+            }
+        }
+    }
+
+    private static void RegisterPipelineBehaviors(IServiceCollection services, IEnumerable<Type> allTypes, ServiceLifetime lifetime)
+    {
+        foreach (var behaviorType in allTypes
+                     .Where(t => t.GetInterfaces().Any(i =>
+                         i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>))))
+        {
+            services.TryAdd(new ServiceDescriptor(behaviorType, behaviorType, lifetime));
+        }
+    }
+
+    internal static void OrderNotificationHandlers<TNotification>(this IServiceCollection services, Type[] handlerTypes, ServiceLifetime serviceLifetime)
+        where TNotification : INotification =>
+        services.TryAdd(new ServiceDescriptor(
+            typeof(IEnumerable<Lazy<INotificationHandler<TNotification>>>),
+            sp => handlerTypes.Select(
+                handlerType => new Lazy<INotificationHandler<TNotification>>(() =>
+                    (INotificationHandler<TNotification>)sp.GetRequiredService(handlerType)
+                )),
+            serviceLifetime));
+
+    private static Func<IServiceProvider, object> BuildLazyFactoryDelegate(Type serviceType)
+    {
+        // Expression Tree: sp => new Lazy<serviceType>(() => sp.GetRequiredService<serviceType>())
+
+        // 1. Define IServiceProvider parameter
+        var spParam = Expression.Parameter(typeof(IServiceProvider), "sp");
+
+        // 2. Get the MethodInfo for sp.GetRequiredService<T>
+        var getRequiredServiceMethodInfo = typeof(ServiceProviderServiceExtensions).GetMethod(
+            nameof(ServiceProviderServiceExtensions.GetRequiredService),
+            [typeof(IServiceProvider)] // Specify overload taking IServiceProvider
+        );
+
+        if (getRequiredServiceMethodInfo == null)
+            throw new InvalidOperationException("Could not find GetRequiredService method.");
+
+        // 3. Make it generic for the specific serviceType: GetRequiredService<serviceType>
+        var genericGetRequiredServiceMethod = getRequiredServiceMethodInfo.MakeGenericMethod(serviceType);
+
+        // 4. Create the call expression: sp.GetRequiredService<serviceType>()
+        var getServiceCall = Expression.Call(null, genericGetRequiredServiceMethod, spParam); // Static method call
+
+        // 5. Create the inner lambda: () => sp.GetRequiredService<serviceType>()
+        //    The delegate type Func<serviceType> is inferred.
+        var innerLambda = Expression.Lambda(getServiceCall);
+
+        // 6. Get the constructor for Lazy<serviceType>(Func<serviceType> factory)
+        var lazyType = typeof(Lazy<>).MakeGenericType(serviceType);
+        var lazyConstructor = lazyType.GetConstructor([innerLambda.Type]); // Get constructor matching Func<T>
+
+        if (lazyConstructor == null)
+            throw new InvalidOperationException($"Could not find constructor for Lazy<{serviceType.Name}> that accepts a factory delegate.");
+
+        // 7. Create the 'new Lazy<serviceType>(innerLambda)' expression
+        var lazyCreation = Expression.New(lazyConstructor, innerLambda);
+
+        // 8. Create the outer lambda: sp => (object)new Lazy<serviceType>(...)
+        //    The result needs to be object type for the ServiceDescriptor factory.
+        var outerLambda = Expression.Lambda<Func<IServiceProvider, object>>(
+            Expression.Convert(lazyCreation, typeof(object)), // Convert Lazy<T> to object
+            spParam
+        );
+
+        // 9. Compile the expression tree into a delegate
+        return outerLambda.Compile();
     }
 }
